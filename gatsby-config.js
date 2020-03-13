5b23dd75841938723ccf1f8ef6ebafa0cd5be0f7
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-03-13 11:42:12 +0000

    Set up project and dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -6,6 +10,20 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: `stq0zqhl1wcv`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`raleway\:100,300,400,400i,700`],
+      },
+      display: "swap",
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
