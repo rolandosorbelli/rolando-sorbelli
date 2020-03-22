ff85f99e18d5af38f314a5e29e3d607e52213379
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-03-22 18:20:37 +0000

    Added Slide

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,23 +1,48 @@
 import React from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 import work_1 from "../../images/work_1.png"
 
-const Work = () => {
-  return (
-    <div className="work">
-      <div className="work__entry--wrapper">
-        <div className="work__entry--promo">
-          <h2>Lorem Ipsum</h2>
-          <div className="work__entry--promo--inner">
-            <img src={work_1} alt="" />
-            <span>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-            </span>
-          </div>
+class Work extends React.Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: false,
+      arrows: false,
+    }
+    return (
+      <div className="work">
+        <div className="work__entry--wrapper">
+          <Slider {...settings}>
+            <div className="work__entry--promo">
+              <h2>Lorem Ipsum</h2>
+              <div className="work__entry--promo--inner">
+                <img src={work_1} alt="" />
+                <span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </span>
+              </div>
+            </div>
+            <div className="work__entry--promo">
+              <h2>Lorem Ipsum</h2>
+              <div className="work__entry--promo--inner">
+                <img src={work_1} alt="" />
+                <span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </span>
+              </div>
+            </div>
+          </Slider>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Work
