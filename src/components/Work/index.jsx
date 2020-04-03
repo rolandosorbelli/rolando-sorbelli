ff9839148985a45aea4a67c5e1e4b689dfbda927
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-04-03 12:41:49 +0100

    Tweaked a links in cards

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,11 +1,25 @@
 import React from "react"
 import Slider from "react-slick"
+import Rodal from "rodal"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
 import work_1 from "../../images/work_1.png"
 
 class Work extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { visible: false }
+  }
+
+  show() {
+    this.setState({ visible: true })
+  }
+
+  hide() {
+    this.setState({ visible: false })
+  }
+
   render() {
     const settings = {
       centerMode: true,
@@ -26,34 +40,43 @@ class Work extends React.Component {
     }
     return (
       <div className="work">
+        <Rodal visible={this.state.visible} onClose={() => this.hide}>
+          <div>Content</div>
+        </Rodal>
         <div className="work__entry--wrapper">
           <Slider {...settings}>
             <div className="work__entry--promo">
-              <h2>Lorem Ipsum</h2>
-              <div className="work__entry--promo--inner">
-                <img src={work_1} alt="" />
-                <span>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-                </span>
-              </div>
+              <a href="/">
+                <h2>Lorem Ipsum</h2>
+                <div className="work__entry--promo--inner">
+                  <img src={work_1} alt="" />
+                  <span>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                  </span>
+                </div>
+              </a>
             </div>
             <div className="work__entry--promo">
-              <h2>Lorem Ipsum</h2>
-              <div className="work__entry--promo--inner">
-                <img src={work_1} alt="" />
-                <span>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-                </span>
-              </div>
+              <a href="/">
+                <h2>Lorem Ipsum</h2>
+                <div className="work__entry--promo--inner">
+                  <img src={work_1} alt="" />
+                  <span>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                  </span>
+                </div>
+              </a>
             </div>
             <div className="work__entry--promo">
-              <h2>Lorem Ipsum</h2>
-              <div className="work__entry--promo--inner">
-                <img src={work_1} alt="" />
-                <span>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-                </span>
-              </div>
+              <a href="/">
+                <h2>Lorem Ipsum</h2>
+                <div className="work__entry--promo--inner">
+                  <img src={work_1} alt="" />
+                  <span>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                  </span>
+                </div>
+              </a>
             </div>
           </Slider>
         </div>
