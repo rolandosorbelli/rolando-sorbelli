e642210b5365f17b9d9153b353685825c5b6ddac
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-04-24 12:26:13 +0100

    Added overlay with image and gradient

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick"
 import Rodal from "rodal"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+import "rodal/lib/rodal.css"
 
 import work_1 from "../../images/work_1.png"
 
@@ -38,23 +39,37 @@ class Work extends React.Component {
         },
       ],
     }
+
+    const modalStyle = {
+      width: "90%",
+      height: "80vh",
+      backgroundColor: "#f6e27f",
+    }
     return (
       <div className="work">
-        <Rodal visible={this.state.visible} onClose={() => this.hide}>
-          <div>Content</div>
-        </Rodal>
         <div className="work__entry--wrapper">
+          <Rodal
+            visible={this.state.visible}
+            onClose={() => this.hide()}
+            animation="slideUp"
+            customStyles={modalStyle}
+          >
+            <div className="overlay--image">
+              <img src={work_1} alt="" />
+            </div>
+            <div className="overlay--content">
+              <div className="gradient"></div>
+            </div>
+          </Rodal>
           <Slider {...settings}>
-            <div className="work__entry--promo">
-              <a href="/">
-                <h2>Lorem Ipsum</h2>
-                <div className="work__entry--promo--inner">
-                  <img src={work_1} alt="" />
-                  <span>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-                  </span>
-                </div>
-              </a>
+            <div className="work__entry--promo" onClick={() => this.show()}>
+              <h2>Lorem Ipsum</h2>
+              <div className="work__entry--promo--inner">
+                <img src={work_1} alt="" />
+                <span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </span>
+              </div>
             </div>
             <div className="work__entry--promo">
               <a href="/">
