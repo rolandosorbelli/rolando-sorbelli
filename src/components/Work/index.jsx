2b97b220783c00fa6a9dcc6b63bb2ef81edc0765
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-04-25 22:57:33 +0100

    Edited close modal icon

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -65,6 +65,7 @@ class Work extends React.Component {
             showMask={true}
           >
             <div className="overlay--image">
+              <img src={bookmark} className="bookmark" />
               <img src={work_1} alt="" />
               <h1>Lorem Ipsum</h1>
             </div>
