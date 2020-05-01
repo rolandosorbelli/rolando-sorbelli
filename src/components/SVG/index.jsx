fa627eb69d8e5a24b36fc99105ce7586c618aa9c
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-05-01 16:26:57 +0100

    Sorted slide and buttons on desktop

--- src/components/SVG/index.jsx
+++ src/components/SVG/index.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 
-const SVG = ({ path, fill, className }) => {
+const SVG = ({ path, fill, className, height, width }) => {
   return (
     <div className="waves">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 319"
         className={className}
+        height={height}
+        width={width}
       >
         <path fill={fill} fill-opacity="1" d={path}></path>
       </svg>
