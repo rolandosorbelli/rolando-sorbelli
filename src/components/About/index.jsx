13d3e7830c8823b5f90727d4c215ee867dc18bb1
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-06-05 20:08:57 +0100

    Refactored about text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -20,16 +20,23 @@ class About extends Component {
           <h2>Software Engineer</h2>
         </div>
         <div className="about__content" data-aos="fade-down">
-          <p>Hi, I'm Rolando, nice to meet you!</p>
-          <p>
+          <p className="about__content--paragraph">
+            Hi there, I'm Rolando, nice to meet you!
+          </p>
+          <p className="about__content--paragraph">
             I am a passionate software engineer who loves creating user-friendly
             experiences. I started my professional journey as a developer in
             2017 when I attended{" "}
             <a href="https://makers.tech/">Makers Academy</a>, a coding bootcamp
             that literally changed my career and gave a positive turn to my
-            life. I am now based in London and I work at BBC as a Software
-            Engineer. Feel free to see my projects and to drop me a message if
-            you feel like!
+            life. I am now based in London and I work at the BBC as a Software
+            Engineer where I have the chance to build products for both internal
+            and external use.
+          </p>
+          <p className="about__content--paragraph">
+            I mostly focus on technologies such as <span>JavaScript</span>,{" "}
+            <span>Node.js</span>, <span>React</span> and <span>HTML & CSS</span>
+            .
           </p>
         </div>
       </div>
