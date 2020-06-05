4d352eeede1118f6ad2d62e3a12b954c641f7086
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-06-05 15:56:52 +0100

    Added about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -20,12 +20,16 @@ class About extends Component {
           <h2>Software Engineer</h2>
         </div>
         <div className="about__content" data-aos="fade-down">
+          <p>Hi, I'm Rolando, nice to meet you!</p>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+            I am a passionate software engineer who loves creating user-friendly
+            experiences. I started my professional journey as a developer in
+            2017 when I attended{" "}
+            <a href="https://makers.tech/">Makers Academy</a>, a coding bootcamp
+            that literally changed my career and gave a positive turn to my
+            life. I am now based in London and I work at BBC as a Software
+            Engineer. Feel free to see my projects and to drop me a message if
+            you feel like!
           </p>
         </div>
       </div>
